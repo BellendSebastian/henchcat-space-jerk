c589121ca283be587717f0e82611bb9ac18395eb
Bellend Sebastian <bellend@bellendsebastian.com>
2014-04-06 11:00:06 +0100

    more

--- src/js/entities/Planet.js
+++ src/js/entities/Planet.js
@@ -1,5 +1,6 @@
 var BaseEntity = require('./BaseEntity');
 var THREE = require('../vendor/three');
+var THREEx = require('../vendor/threex.atmospherematerial');
 
 Planet.prototype = new BaseEntity();
 Planet.prototype.constructor = Planet;
@@ -10,17 +11,22 @@ function Planet() {
 
     var geometry = new THREE.SphereGeometry(50, 32, 32);
     var material = new THREE.MeshPhongMaterial({
-        map: THREE.ImageUtils.loadTexture('img/earthmap1k.jpg'),
-        bumpMap: THREE.ImageUtils.loadTexture('img/earthbump1k.jpg'),
-        bumpScale: 0.05,
+        map: THREE.ImageUtils.loadTexture('img/marsmap1k.jpg'),
+        bumpMap: THREE.ImageUtils.loadTexture('img/marsbump1k.jpg'),
+        bumpScale: 0.5/*,
         specularMap: THREE.ImageUtils.loadTexture('img/earthspec1k.jpg'),
-        specular: new THREE.Color('grey')
+        specular: new THREE.Color('grey')*/
     });
 
     this.mesh = new THREE.Mesh(geometry, material);
     this.mesh.position = new THREE.Vector3(-40, 20, 0);
 
-    var cloudGeometry = new THREE.SphereGeometry(50.5, 32, 32);
+    var glowGeometry = new THREE.SphereGeometry(50.1, 32, 32);
+    var glowMaterial = THREEx.createAtmosphereMaterial();
+    var meshHalo = new THREE.Mesh(glowGeometry, glowMaterial);
+    this.mesh.add(meshHalo);
+
+    var cloudGeometry = new THREE.SphereGeometry(54.5, 32, 32);
     var cloudMaterial = new THREE.MeshPhongMaterial({
         map: THREE.ImageUtils.loadTexture('img/earthcloudmap.jpg'),
         side: THREE.DoubleSide,
@@ -30,7 +36,7 @@ function Planet() {
     });
 
     this.cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial);
-    this.mesh.add(this.cloudMesh);
+    //this.mesh.add(this.cloudMesh);
 }
 
 Planet.prototype.update = function () {
