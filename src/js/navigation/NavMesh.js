c14f85dae77e7cbefd559cab1488224d87f7c695
Bellend Sebastian <bellend@bellendsebastian.com>
2014-04-22 13:17:55 +0100

    fixing navmesh clicking

--- src/js/navigation/NavMesh.js
+++ src/js/navigation/NavMesh.js
@@ -3,7 +3,7 @@ var CONFIG = require('../config');
 function NavMesh(renderer) {
     'use strict';
     this.grid = [];
-    this.meshSize = 100;
+    this.meshSize = 75;
     for (var y = 0; y < CONFIG.sectorHeight / this.meshSize * 2; y++) {
         this.grid[y] = [];
         for (var x = 0; x < CONFIG.sectorWidth / this.meshSize * 2; x++) {
@@ -19,7 +19,7 @@ function NavMesh(renderer) {
 NavMesh.prototype.mouseDown = function (e) {
     'use strict';
     e.preventDefault();
-    var vector = new THREE.Vector3((e.clientX / window.innerWidth) * 2 - 1, (e.clientY / window.innerHeight) * 2 + 1, 0.5);
+    var vector = new THREE.Vector3((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1, 0.5);
     this.projector.unprojectVector(vector, this.renderer.camera);
     var caster = new THREE.Raycaster(this.renderer.camera.position, vector.sub(this.renderer.camera.position).normalize());
     var intersects = caster.intersectObjects(this.renderer.scene.children);
